9b236137181b42bda988b6912c265d731bff9a54
marshmellow-sys <1160788607@qq.com>
2019-11-26 15:52:51 +0800

    第一次修改

---
+++ src/com/fky/Gogo.java
@@ -0,0 +1,12 @@
+package com.fky;
+
+import java.sql.SQLOutput;
+
+/**
+ * marshmellow write this in 2019/11/26
+ */
+public class Gogo {
+    public static void main(String [] args){
+        System.out.println("第二个项目");
+    }
+}
