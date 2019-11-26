60cdf8f880dc693a6f6c55219ad87abf9df57aa5
marshmellow-sys <1160788607@qq.com>
2019-11-26 16:48:38 +0800

    master

---
+++ src/com/fky/Master.java
@@ -0,0 +1,10 @@
+package com.fky;
+
+/**
+ * marshmellow write this in 2019/11/26
+ */
+public class Master {
+    public static void main(String [] args){
+        System.out.println("master-----");
+    }
+}
